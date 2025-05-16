7e7ef5dcc440878962a30ff96eae10b31c7d39e7
JAIME RESTREPO <jaime.restrepo413@pascualbravo.edu.co>
2025-05-16 04:38:30 -0600

    Add files via upload

--- SistemaGestion/SistemaGestion/IExportacion.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Data;
-
-namespace SistemaGestion.Exportadores
-{
-    public interface IExportacion
-    {
-        void Exportar(IDataReader reader, string filePath, string query);
-    }
-}
